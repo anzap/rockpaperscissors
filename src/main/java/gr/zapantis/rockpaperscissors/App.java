d7661d29c49046091ebea3a46507290cb4de779c
Andreas <Andreas@Andreas-PC>
2012-06-17 18:53:57 +0300

    Creation of project

---
+++ src/main/java/gr/zapantis/rockpaperscissors/App.java
@@ -0,0 +1,13 @@
+package gr.zapantis.rockpaperscissors;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
