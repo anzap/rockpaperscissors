3d8d978d01b97f18f73e17cbe5e1da1fd5bcad0e
Andreas <Andreas@Andreas-PC>
2012-06-17 19:00:30 +0300

    Added basic test cases for the rules as specified in the case description.

---
+++ src/test/java/gr/zapantis/rockpaperscissors/RulesTest.java
@@ -0,0 +1,29 @@
+package gr.zapantis.rockpaperscissors;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class RulesTest {
+
+	@Test
+	public void scissorsBeatsPaper() {
+		fail("Not implemented yet");
+	}
+
+	@Test
+	public void rockBeatsScissors() {
+		fail("Not implemented yet");
+	}
+
+	@Test
+	public void paperBeatsRock() {
+		fail("Not implemented yet");
+	}
+	
+	@Test
+	public void equalMovesAreATie() {
+		fail("Not implemented yet");
+	}
+
+}
