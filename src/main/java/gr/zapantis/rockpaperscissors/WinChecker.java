3da5d1c54fd107c39458d10683e880841c75b86a
Andreas <Andreas@Andreas-PC>
2012-06-17 19:27:11 +0300

    Added the logic in WinChecker to calculate the outcome of a game round.

--- src/main/java/gr/zapantis/rockpaperscissors/WinChecker.java
+++ src/main/java/gr/zapantis/rockpaperscissors/WinChecker.java
@@ -1,12 +1,30 @@
 package gr.zapantis.rockpaperscissors;
 
-
 public final class WinChecker {
-	
+
 	public static Outcome evaluateWinner(Move firstMove, Move secondMove) {
 		Outcome outcome = Outcome.TIE;
-		return outcome;
 		
+		/*
+		 * Game logic for the winning move.
+		 * 
+		 * Scissors beats paper.
+		 * Rock beats scissors.
+		 * Paper beats rock.
+		 * If both player choose the same, the outcome is a tie.
+		 */
+		if ((Move.PAPER.equals(firstMove) && Move.ROCK.equals(secondMove))
+				|| (Move.SCISSORS.equals(firstMove) && Move.PAPER.equals(secondMove))
+				|| (Move.ROCK.equals(firstMove) && Move.SCISSORS.equals(secondMove))) {
+			outcome = Outcome.WIN;
+		} else if ((Move.ROCK.equals(firstMove) && Move.PAPER.equals(secondMove))
+				|| (Move.PAPER.equals(firstMove) && Move.SCISSORS.equals(secondMove))
+				|| (Move.SCISSORS.equals(firstMove) && Move.ROCK.equals(secondMove))) {
+			outcome = Outcome.LOSS;
+		}
+
+		return outcome;
+
 	}
 
 }
