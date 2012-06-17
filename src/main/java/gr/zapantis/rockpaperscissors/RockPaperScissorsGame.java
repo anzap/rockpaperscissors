df1b75109b0bd41d021945c252510e43d2cad734
Andreas <Andreas@Andreas-PC>
2012-06-17 20:16:52 +0300

    Refactored game class to set players in constructor. Refactored game class test case, added setup method for common operations between different test methods.

--- src/main/java/gr/zapantis/rockpaperscissors/RockPaperScissorsGame.java
+++ src/main/java/gr/zapantis/rockpaperscissors/RockPaperScissorsGame.java
@@ -1,12 +1,16 @@
 package gr.zapantis.rockpaperscissors;
 
 public class RockPaperScissorsGame {
+	
+	private final Player firstPlayer;
+	private final Player secondPlayer;
 
-	public Outcome round(Player firstPlayer, Player secondPlayer) {
-		return calculateWinner(firstPlayer, secondPlayer);
+	public RockPaperScissorsGame(Player firstPlayer, Player secondPlayer) {
+		this.firstPlayer = firstPlayer;
+		this.secondPlayer = secondPlayer;
 	}
 
-	private Outcome calculateWinner(Player firstPlayer, Player secondPlayer) {
+	public Outcome round() {
 		return WinChecker.evaluateWinner(firstPlayer.getMove(), secondPlayer.getMove());
 	}
 
--- src/test/java/gr/zapantis/rockpaperscissors/RockPaperScissorsGameTest.java
+++ src/test/java/gr/zapantis/rockpaperscissors/RockPaperScissorsGameTest.java
@@ -3,20 +3,25 @@ package gr.zapantis.rockpaperscissors;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RockPaperScissorsGameTest {
+	private RockPaperScissorsGame game;
+	
+	@Before
+	public void setup() {
+		game = new RockPaperScissorsGame(new Player("A", Move.PAPER), new Player("B", Move.ROCK));
+	}
 	
 	@Test
 	public void createGame() {
-		RockPaperScissorsGame rockPaperScissorsGame = new RockPaperScissorsGame();
-		assertNotNull(rockPaperScissorsGame);
+		assertNotNull(game);
 	}
 	
 	@Test
 	public void testGameRound() {
-		RockPaperScissorsGame game = new RockPaperScissorsGame();
-		assertEquals(Outcome.WIN, game.round(new Player("A", Move.PAPER), new Player("B", Move.ROCK)));
+		assertEquals(Outcome.WIN, game.round());
 	}
 
 
