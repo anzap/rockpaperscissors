1c4586d444089e5e6f15e15c6dffbf1a4dfd304a
Andreas <Andreas@Andreas-PC>
2012-06-17 19:50:54 +0300

    Added round() method to game class to represent a game round between two players.

--- src/main/java/gr/zapantis/rockpaperscissors/Player.java
+++ src/main/java/gr/zapantis/rockpaperscissors/Player.java
@@ -1,19 +1,19 @@
 package gr.zapantis.rockpaperscissors;
 
 public class Player {
-	
+
 	private String name;
 	private final Move move;
-	
+
 	public Player(String name, Move move) {
 		this.name = name;
 		this.move = move;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public Move getMove() {
 		return move;
 	}
--- src/main/java/gr/zapantis/rockpaperscissors/RockPaperScissorsGame.java
+++ src/main/java/gr/zapantis/rockpaperscissors/RockPaperScissorsGame.java
@@ -2,8 +2,12 @@ package gr.zapantis.rockpaperscissors;
 
 public class RockPaperScissorsGame {
 	
-	public void round(Player firstPlayer, Player secondPlayer) {
-		
+	public Outcome round(Player firstPlayer, Player secondPlayer) {
+		return calculateWinner(firstPlayer, secondPlayer);
+	}
+
+	private Outcome calculateWinner(Player firstPlayer, Player secondPlayer) {
+		return WinChecker.evaluateWinner(firstPlayer.getMove(), secondPlayer.getMove());
 	}
 
 }
